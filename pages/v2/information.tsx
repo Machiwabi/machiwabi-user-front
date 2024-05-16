ab2d8fe3a7d03554fe9062fadfc879e31257d302
hanzochang <hanzo.chan@gmail.com>
2024-05-16 19:49:11 +0900

    feat event tempalte

--- pages/v2/information.tsx
+++ pages/v2/information.tsx
@@ -1,12 +1,9 @@
-import { Box, Container, Flex } from '@mantine/core'
-import { Fragment } from 'react'
-import { EButton } from '../../componentsNew/elements/EButton'
-import { EHeading } from '../../componentsNew/elements/EHeading/base'
-import { EText } from '../../componentsNew/elements/EText/base'
+import { Container } from '@mantine/core'
 import { OFooterNav } from '../../componentsNew/organisms/OFooterNav'
 import { OHeaderNav } from '../../componentsNew/organisms/OHeaderNav'
 import { OWaitingHeader } from '../../componentsNew/organisms/OWaitingHeader'
 import { OWaitingTabs } from '../../componentsNew/organisms/OWaitingTabs'
+import { TEventShowTemplate } from '../../componentsNew/templates/TEventShowTemplate'
 import { eventMock } from '../../mocks/event.mock'
 
 const Page = () => {
@@ -17,58 +14,7 @@ const Page = () => {
         <OWaitingHeader mt={24} px={16} />
         <OWaitingTabs px={16} current="INFORMATION" />
 
-        <Box mt={24} mb={16} px={16}>
-          <EHeading.Page>イベント名</EHeading.Page>
-          <Flex align="center" my={4}>
-            <Box className="material-icons-outlined" component="i" fz={16}>
-              location_on
-            </Box>
-            <Box pb={2} fz={10}>
-              {eventMock.placeName}
-            </Box>
-          </Flex>
-        </Box>
-
-        <Box mb={40} px={16}>
-          <EText.Desc1>{eventMock.description}</EText.Desc1>
-        </Box>
-
-        <Box my={40} px={16}>
-          <EHeading.Section>イベント詳細</EHeading.Section>
-          <EText.Desc1 mt={8}>
-            {eventMock.detailMdxContent &&
-              eventMock.detailMdxContent.split('\n').map((line, index) => (
-                <Fragment key={index}>
-                  {line}
-                  <br />
-                </Fragment>
-              ))}
-          </EText.Desc1>
-        </Box>
-
-        <Box my={40} px={16}>
-          <EHeading.Section>開催場所</EHeading.Section>
-          <EText.Desc1 mt={8}>{eventMock.placeName}</EText.Desc1>
-        </Box>
-
-        <Box my={40} px={16}>
-          <EHeading.Section>地図</EHeading.Section>
-          <Box mt={8}>
-            <iframe
-              src={`https://www.google.com/maps?q=${eventMock.placeName}@${eventMock.lat},${eventMock.lng}&z=15&output=embed`}
-              width="100%"
-              height="410"
-              loading="lazy"
-              style={{ border: 0 }}
-            />
-            <EButton.Sm w="100%">大きな地図で見る</EButton.Sm>
-          </Box>
-        </Box>
-
-        <Box my={40} px={16}>
-          <EHeading.Section>オンライン</EHeading.Section>
-          <EText.Desc1 mt={8}>{eventMock.onlineUrl}</EText.Desc1>
-        </Box>
+        <TEventShowTemplate event={eventMock} />
       </Container>
       <OFooterNav />
     </>
