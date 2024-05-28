56c796a743082a386d0e76f2fcafa4d8dfd7cd2c
hanzochang <hanzo.chan@gmail.com>
2024-05-28 21:59:24 +0900

    feat tiny revises

--- components/04_screens/SEventScreen/index.tsx
+++ components/04_screens/SEventScreen/index.tsx
@@ -1,16 +1,15 @@
-import { Box, Flex } from '@mantine/core'
+import { Flex } from '@mantine/core'
 import { FC } from 'react'
 import { applicationProperties } from '../../../constants/applicationProperties'
 import { eventUrl, waitingUrl } from '../../../helpers/url.helper'
 import { useEvent } from '../../../hooks/resources/useEvent'
+import { useJoinWaiting } from '../../../hooks/resources/useJoinWaiting'
 import { useWeb3Auth } from '../../../hooks/useWeb3Auth'
 import { useAuthenticatedStore } from '../../../recoil/authenticatedStore/useAuthenticatedStore'
-import { EButton } from '../../01_elements/EButton'
+import { OEventJoinButton } from '../../02_organisms/OEventJoinButton'
 import { TErrorTemplate } from '../../03_templates/TErrorTemplate'
 import { TEventShowTemplate } from '../../03_templates/TEventShowTemplate'
 import { TLoadingTemplate } from '../../03_templates/TLoadingTemplate'
-import { useJoinWaiting } from '../../../hooks/resources/useJoinWaiting'
-import { OEventJoinButton } from '../../02_organisms/OEventJoinButton'
 
 type Props = {
   uniqueKey: string
@@ -20,16 +19,6 @@ const Component: FC<Props> = ({ uniqueKey }) => {
   const { event, eventError, eventIsLoading } = useEvent({
     uniqueKey,
   })
-
-  const { createJoinWaiting } = useJoinWaiting()
-
-  const submit = async () => {
-    const joinWaiting = await createJoinWaiting({ eventUniqueKey: uniqueKey })
-    window.location.href = waitingUrl(joinWaiting.joinEvent.uniqueKey)
-  }
-
-  const { isAuthenticated } = useAuthenticatedStore()
-  const { connectWeb3AuthAndSignInWithEthereum } = useWeb3Auth()
 
   if (eventError) return <TErrorTemplate />
   if (eventIsLoading || !event) return <TLoadingTemplate />
@@ -52,51 +41,6 @@ const Component: FC<Props> = ({ uniqueKey }) => {
             uniqueKey
           )}`}
         />
-        {/* {isAuthenticated() ? (
-          <>
-            <Box w="100%" maw={410} px={16}>
-              <EButton.Lg
-                w="100%"
-                fillType="filled"
-                surface="surface3"
-                onClick={submit}
-              >
-                参加する
-              </EButton.Lg>
-            </Box>
-          </>
-        ) : (
-          <>
-            {event.isJoinable ? (
-              <>
-                <Box w="100%" maw={410} px={16}>
-                  <EButton.Lg
-                    w="100%"
-                    fillType="filled"
-                    surface="surface3"
-                    onClick={() => {
-                      connectWeb3AuthAndSignInWithEthereum(
-                        `${applicationProperties.HOSTING_URL}${eventUrl(
-                          uniqueKey
-                        )}`
-                      )
-                    }}
-                  >
-                    ログインして参加する
-                  </EButton.Lg>
-                </Box>
-              </>
-            ) : (
-              <>
-                <Box w="100%" maw={410} px={16}>
-                  <EButton.Lg w="100%" fillType="disabled" disabled>
-                    終了しました
-                  </EButton.Lg>
-                </Box>
-              </>
-            )}
-          </>
-        )} */}
       </Flex>
     </>
   )
