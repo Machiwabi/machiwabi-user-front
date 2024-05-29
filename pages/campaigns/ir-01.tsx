f885ed7e32ec115784ad4142f9086a48f9f1a4ac
hanzochang <hanzo.chan@gmail.com>
2024-05-29 20:15:11 +0900

    feat lp create features

--- pages/campaigns/ir-01.tsx
+++ pages/campaigns/ir-01.tsx
@@ -1,5 +1,6 @@
 import LLandingPageLayout from '../../components/00_layouts/LLandingPageLayout'
 import { BgSection } from '../../components/99_lp/BgSection'
+import { FeatureSection } from '../../components/99_lp/FeatureSection'
 import { HeadingSection } from '../../components/99_lp/HeadingSection'
 import { HeroSection } from '../../components/99_lp/HeroSection'
 import { StickyHeaderSection } from '../../components/99_lp/StickyHeaderSection'
@@ -12,6 +13,7 @@ const Page: NextPageWithLayout = () => {
       {/* <BgSection /> */}
       <HeadingSection mt={120} mb={80} />
       <HeroSection mt={120} mb={80} />
+      <FeatureSection my={{ base: 62, lg: 300 }} />
     </>
   )
 }
