21deac4740a3bdd50a37f46e67253b5b51fe4f1f
hanzochang <hanzo.chan@gmail.com>
2024-06-01 00:20:20 +0900

    feat setup terms

--- components/04_screens/SOverlayedMenuScreen/OverlayedMenuList.tsx
+++ components/04_screens/SOverlayedMenuScreen/OverlayedMenuList.tsx
@@ -1,7 +1,8 @@
 import { BoxProps, Flex } from '@mantine/core'
 import { FC } from 'react'
 import { OverlayedMenuListItem } from './OverlayedMenuListItem'
-import { web3AuthSignOutUrl } from '../../../helpers/url.helper'
+import { waitingsUrl, web3AuthSignOutUrl } from '../../../helpers/url.helper'
+import { applicationUrls } from '../../../constants/applicationUrls'
 
 type Props = BoxProps
 
@@ -9,11 +10,18 @@ const Component: FC<Props> = ({ ...props }) => {
   return (
     <>
       <Flex justify="center" direction="column" w="100%" {...props}>
+        <OverlayedMenuListItem href={waitingsUrl()} title="ホーム画面" />
         <OverlayedMenuListItem href="/" title="使い方" />
-        <OverlayedMenuListItem href="/" title="お問い合せ" />
-        <OverlayedMenuListItem href="/posts" title="特定商取引法に基づく表記" />
-        <OverlayedMenuListItem href="/posts" title="利用規約" />
-        <OverlayedMenuListItem href="/" title="プライバシーポリシー" />
+        <OverlayedMenuListItem href={applicationUrls.form} title="お問い合せ" />
+        <OverlayedMenuListItem
+          href={applicationUrls.tokusho}
+          title="特定商取引法に基づく表記"
+        />
+        <OverlayedMenuListItem href={applicationUrls.terms} title="利用規約" />
+        <OverlayedMenuListItem
+          href={applicationUrls.privacy}
+          title="プライバシーポリシー"
+        />
         <OverlayedMenuListItem href={web3AuthSignOutUrl()} title="ログアウト" />
       </Flex>
     </>
