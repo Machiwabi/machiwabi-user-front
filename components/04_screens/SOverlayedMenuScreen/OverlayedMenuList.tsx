479ccb1312324879b856c05c89b9c5195b34db8a
hanzochang <hanzo.chan@gmail.com>
2024-06-01 19:08:11 +0900

    feat revise menulist

--- components/04_screens/SOverlayedMenuScreen/OverlayedMenuList.tsx
+++ components/04_screens/SOverlayedMenuScreen/OverlayedMenuList.tsx
@@ -1,7 +1,11 @@
-import { BoxProps, Flex } from '@mantine/core'
+import { Box, BoxProps, Flex } from '@mantine/core'
 import { FC } from 'react'
 import { OverlayedMenuListItem } from './OverlayedMenuListItem'
-import { waitingsUrl, web3AuthSignOutUrl } from '../../../helpers/url.helper'
+import {
+  userEditUrl,
+  waitingsUrl,
+  web3AuthSignOutUrl,
+} from '../../../helpers/url.helper'
 import { applicationUrls } from '../../../constants/applicationUrls'
 
 type Props = BoxProps
@@ -11,6 +15,11 @@ const Component: FC<Props> = ({ ...props }) => {
     <>
       <Flex justify="center" direction="column" w="100%" {...props}>
         <OverlayedMenuListItem href={waitingsUrl()} title="ホーム画面" />
+        <OverlayedMenuListItem
+          href={userEditUrl()}
+          title="アカウント情報の編集"
+        />
+
         <OverlayedMenuListItem href={applicationUrls.howTo} title="使い方" />
         <OverlayedMenuListItem href={applicationUrls.form} title="お問い合せ" />
         <OverlayedMenuListItem
