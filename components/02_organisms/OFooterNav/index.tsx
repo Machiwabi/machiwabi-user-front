c4a22c0b2e75bf21398e28352470992d93e84f25
hanzochang <hanzo.chan@gmail.com>
2024-06-01 19:31:49 +0900

    feat create footer button

--- components/02_organisms/OFooterNav/index.tsx
+++ components/02_organisms/OFooterNav/index.tsx
@@ -1,10 +1,7 @@
 import { Box, Flex } from '@mantine/core'
 import { FC, useState } from 'react'
-import { colorScheme } from '../../../theme/colorScheme'
-import Link from 'next/link'
-import styles from './style.module.scss'
-import { userEditUrl, waitingsUrl } from '../../../helpers/url.helper'
 import { useAuthenticatedStore } from '../../../recoil/authenticatedStore/useAuthenticatedStore'
+import { colorScheme } from '../../../theme/colorScheme'
 import { TModalGuidePwaTemplate } from '../../03_templates/TModalGuidePwaTemplate'
 
 const Component: FC = () => {
@@ -14,87 +11,33 @@ const Component: FC = () => {
 
   return (
     <>
-      <Box pos="fixed" w="100%" left={0} bottom={0}>
-        <Flex w="100%" h={80}>
-          <Flex
-            w={`calc(50% - ${96 / 2}px)`}
-            h="100%"
-            bg={colorScheme.scheme1.surface2.surface}
-            justify="end"
-            align="center"
-          >
-            <Link
-              href={waitingsUrl()}
-              className={styles['o-footer-nav__menu-link']}
-            >
-              <Flex
-                component="i"
-                className={`material-icons-outlined`}
-                align="center"
-                h="100%"
-                px={24 * 2}
-                py={24}
-                fz={24}
-                c={colorScheme.scheme1.surface1.object.high}
-              >
-                home
-              </Flex>
-            </Link>
-          </Flex>
-          <Box pos="relative" w={96} h="100%">
-            <GuidePwaScreen />
-          </Box>
-          <Flex
-            w={`calc(50% - ${96 / 2}px)`}
-            h="100%"
-            bg={colorScheme.scheme1.surface2.surface}
-            justify="start"
-            align="center"
-          >
-            <Link
-              href={userEditUrl()}
-              className={styles['o-footer-nav__menu-link']}
-            >
-              <Flex
-                component="i"
-                className="material-icons-outlined"
-                align="center"
-                h="100%"
-                px={24 * 2}
-                py={24}
-                fz={24}
-                c={colorScheme.scheme1.surface1.object.high}
-              >
-                account_circle
-              </Flex>
-            </Link>
-          </Flex>
-        </Flex>
-      </Box>
+      <GuideFloatButton />
     </>
   )
 }
 
 export { Component as OFooterNav }
 
-const GuidePwaScreen: FC = () => {
+const GuideFloatButton: FC = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
-      {/* <Link href="/" className={styles['o-footer-nav__menu-link']}> */}
       <Flex
-        pos="absolute"
+        pos="fixed"
         justify="center"
         align="center"
-        left={(96 - 68) / 2}
-        top={-68 / 2}
+        right={16}
+        bottom={48}
         bg={colorScheme.scheme1.surface3.surface}
         w={68}
         h={68}
-        style={{ borderRadius: 68 / 2 }}
         onClick={() => {
           setIsOpen(true)
+        }}
+        style={{
+          borderRadius: 68 / 2,
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
         }}
       >
         <Box
@@ -108,20 +51,6 @@ const GuidePwaScreen: FC = () => {
           add_circle
         </Box>
       </Flex>
-      <svg
-        width="96"
-        height="80"
-        viewBox="0 0 96 80"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M48 48C74.5097 48 96 26.5097 96 0V80H0V0C0 26.5097 21.4903 48 48 48Z"
-          fill={colorScheme.scheme1.surface2.surface}
-        />
-      </svg>
       <TModalGuidePwaTemplate isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   )
