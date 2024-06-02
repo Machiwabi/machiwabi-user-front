65bc2b37b4e2cfe225ac50c2583b7e990bbb2c5e
hanzochang <hanzo.chan@gmail.com>
2024-06-02 16:13:20 +0900

    feat add description when signin

--- components/02_organisms/OEventJoinButton/index.tsx
+++ components/02_organisms/OEventJoinButton/index.tsx
@@ -8,6 +8,7 @@ import { useWeb3Auth } from '../../../hooks/useWeb3Auth'
 import { useAuthenticatedStore } from '../../../recoil/authenticatedStore/useAuthenticatedStore'
 import { EButton } from '../../01_elements/EButton'
 import { ELoader } from '../../01_elements/ELoader'
+import { colorScheme } from '../../../theme/colorScheme'
 
 type Props = {
   event: EventEntity
@@ -37,8 +38,26 @@ const Component: FC<Props> = ({ event, redirectUrl }) => {
                     connectWeb3AuthAndSignInWithEthereum(redirectUrl)
                   }}
                 >
-                  ログインして参加する
+                  新規作成／ログインして参加する
                 </EButton.Lg>
+                <Box
+                  mt={8}
+                  p={8}
+                  bg={colorScheme.scheme1.surface1.surface}
+                  c={colorScheme.scheme1.surface1.object.high}
+                  style={{
+                    borderRadius: 8,
+                    border: `1px solid ${colorScheme.scheme1.surface1.object.high}`,
+                  }}
+                >
+                  <Box fz={10}>
+                    ・ safariやchromeなどのブラウザでご利用ください。
+                  </Box>
+                  <Box mt={2} fz={10}>
+                    ・
+                    ウォレットご利用の方：「接続」と「署名」の２回操作があります。
+                  </Box>
+                </Box>
               </Box>
             </>
           ) : (
