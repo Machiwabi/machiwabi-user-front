202991f2510ead31c995cb3bd9c4167d90092fe0
hanzochang <hanzo.chan@gmail.com>
2024-06-23 10:12:46 +0900

    fixed bug

--- domains/services/event.service.ts
+++ domains/services/event.service.ts
@@ -3,8 +3,12 @@ import { EventEntity } from '../../generated/graphql'
 export class EventService {
   constructor(private event: EventEntity) {}
 
+  public eventWaitingStarted(baseDate: Date = new Date()) {
+    return new Date(this.event.waitingStartAt) < baseDate
+  }
+
   public eventStarted(baseDate: Date = new Date()) {
-    return new Date(this.event.startAt) > baseDate
+    return new Date(this.event.startAt) < baseDate
   }
 
   public eventEnded(baseDate: Date = new Date()) {
