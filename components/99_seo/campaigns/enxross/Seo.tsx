92d764ee0500f885d63b35cc2333f22af7e7c5b2
hanzochang <hanzo.chan@gmail.com>
2024-06-26 23:47:44 +0900

    revise ogp

--- components/99_seo/campaigns/enxross/Seo.tsx
+++ components/99_seo/campaigns/enxross/Seo.tsx
@@ -9,12 +9,12 @@ const Component: FC = () => {
       <NextSeo
         title={'Mati-wabi マチワビ - 待ち時間を価値するアプリ'}
         description={
-          'イベントまでの“待ち侘びる時間”が推しの応援になったら？Mati-wabiはあなたの推し活時間をもっと価値のあるものにする「待ち活」専用アプリです！'
+          'イベントまでの“待ち侘びる時間”が価値になったら？Mati-wabiはあなたの待ち時間をもっと価値のあるものにする「待ち活」専用アプリです！'
         }
         openGraph={{
           title: 'Mati-wabi マチワビ - 待ち時間を価値するアプリ',
           description:
-            'イベントまでの“待ち侘びる時間”が推しの応援になったら？Mati-wabiはあなたの推し活時間をもっと価値のあるものにする「待ち活」専用アプリです！',
+            'イベントまでの“待ち侘びる時間”が価値になったら？Mati-wabiはあなたの待ち時間をもっと価値のあるものにする「待ち活」専用アプリです！',
           images: [
             {
               url: `${applicationProperties.HOSTING_URL}/assets/ogp/ogp_twitterCard_default.png`,
