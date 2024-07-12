b666b6db1f2b7a4f3fc8a2737cb6386c3ced159f
hanzochang <hanzo.chan@gmail.com>
2024-07-12 12:06:43 +0900

    tmp

--- hooks/usePushNotificationRegistration.ts
+++ hooks/usePushNotificationRegistration.ts
@@ -10,13 +10,15 @@ export const usePushNotificationRegistration = () => {
     if (!siwe) return
 
     if ('Notification' in window) {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === 'granted') {
-          console.log('Notification permission granted.')
-        } else {
-          console.log('Notification permission denied.')
-        }
-      })
+      console.log('Notification?')
+      const permission = await Notification.requestPermission()
+      console.log('permission?', permission)
+      if (permission === 'granted') {
+        console.log('Notification permission granted.')
+      } else {
+        console.log('Notification permission denied.')
+        return
+      }
     }
 
     console.log('check sw')
@@ -56,55 +58,6 @@ export const usePushNotificationRegistration = () => {
       })
     }
     console.log('subscription:', pushManagerSubscription)
-
-    // if ('serviceWorker' in navigator && 'PushManager' in window) {
-    //   navigator.serviceWorker
-    //     .register('/sw.js')
-    //     .then((registration) => {
-    //       console.log(
-    //         'Service Worker registered with scope:',
-    //         registration.scope
-    //       )
-
-    //       return registration.pushManager
-    //         .getSubscription()
-    //         .then((subscription) => {
-    //           if (subscription === null) {
-    //             // サブスクリプションを作成
-    //             // repositoryに移動
-    //             return registration.pushManager.subscribe({
-    //               userVisibleOnly: true,
-    //               applicationServerKey: urlBase64ToUint8Array(
-    //                 applicationProperties.VAPID_PUBLIC_KEY
-    //               ),
-    //             })
-    //           } else {
-    //             return subscription
-    //           }
-    //         })
-    //     })
-    //     .then((subscription: PushSubscription | null) => {
-    //       if (subscription) {
-    //         const stringifiedSubscription: {
-    //           endpoint: string
-    //           expirationTime: number | null
-    //           keys: {
-    //             p256dh: string
-    //             auth: string
-    //           }
-    //         } = JSON.parse(JSON.stringify(subscription))
-
-    //         createUserDevice({
-    //           webPushEndPoint: stringifiedSubscription.endpoint,
-    //           webPushExpirationTime: stringifiedSubscription.expirationTime,
-    //           webPushKey: stringifiedSubscription.keys.p256dh,
-    //           webPushAuth: stringifiedSubscription.keys.auth,
-    //         })
-    //       }
-    //       console.log('subscription:', subscription)
-    //     })
-    //     .catch((error) => console.error('Service Worker Error', error))
-    // }
   }
 
   return { register }
