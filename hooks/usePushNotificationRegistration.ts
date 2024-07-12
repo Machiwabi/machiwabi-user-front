0caf0cec40d1370cb4321172aa83cfae8c80d33b
hanzochang <hanzo.chan@gmail.com>
2024-07-12 11:29:00 +0900

    tmp set logger

--- hooks/usePushNotificationRegistration.ts
+++ hooks/usePushNotificationRegistration.ts
@@ -9,54 +9,92 @@ export const usePushNotificationRegistration = () => {
     const siwe = await SiweJwtRepository.getSiweJwtFromBrowser()
     if (!siwe) return
 
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log(
-            'Service Worker registered with scope:',
-            registration.scope
-          )
+    console.log('check sw')
+    const swRegistration = await navigator.serviceWorker.register('/sw.js')
+    console.log('check pushManagerSubscription')
+    let pushManagerSubscription =
+      await swRegistration.pushManager.getSubscription()
 
-          return registration.pushManager
-            .getSubscription()
-            .then((subscription) => {
-              if (subscription === null) {
-                // サブスクリプションを作成
-                // repositoryに移動
-                return registration.pushManager.subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: urlBase64ToUint8Array(
-                    applicationProperties.VAPID_PUBLIC_KEY
-                  ),
-                })
-              } else {
-                return subscription
-              }
-            })
-        })
-        .then((subscription: PushSubscription | null) => {
-          if (subscription) {
-            const stringifiedSubscription: {
-              endpoint: string
-              expirationTime: number | null
-              keys: {
-                p256dh: string
-                auth: string
-              }
-            } = JSON.parse(JSON.stringify(subscription))
-
-            createUserDevice({
-              webPushEndPoint: stringifiedSubscription.endpoint,
-              webPushExpirationTime: stringifiedSubscription.expirationTime,
-              webPushKey: stringifiedSubscription.keys.p256dh,
-              webPushAuth: stringifiedSubscription.keys.auth,
-            })
-          }
-          console.log('subscription:', subscription)
-        })
-        .catch((error) => console.error('Service Worker Error', error))
+    if (pushManagerSubscription) {
+      console.log('pms not exists--', pushManagerSubscription)
+      pushManagerSubscription = await swRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          applicationProperties.VAPID_PUBLIC_KEY
+        ),
+      })
+    } else {
+      console.log('pms exists--', pushManagerSubscription)
     }
+
+    console.log('save subscription')
+    if (pushManagerSubscription) {
+      const stringifiedSubscription: {
+        endpoint: string
+        expirationTime: number | null
+        keys: {
+          p256dh: string
+          auth: string
+        }
+      } = JSON.parse(JSON.stringify(pushManagerSubscription))
+
+      createUserDevice({
+        webPushEndPoint: stringifiedSubscription.endpoint,
+        webPushExpirationTime: stringifiedSubscription.expirationTime,
+        webPushKey: stringifiedSubscription.keys.p256dh,
+        webPushAuth: stringifiedSubscription.keys.auth,
+      })
+    }
+    console.log('subscription:', pushManagerSubscription)
+
+    // if ('serviceWorker' in navigator && 'PushManager' in window) {
+    //   navigator.serviceWorker
+    //     .register('/sw.js')
+    //     .then((registration) => {
+    //       console.log(
+    //         'Service Worker registered with scope:',
+    //         registration.scope
+    //       )
+
+    //       return registration.pushManager
+    //         .getSubscription()
+    //         .then((subscription) => {
+    //           if (subscription === null) {
+    //             // サブスクリプションを作成
+    //             // repositoryに移動
+    //             return registration.pushManager.subscribe({
+    //               userVisibleOnly: true,
+    //               applicationServerKey: urlBase64ToUint8Array(
+    //                 applicationProperties.VAPID_PUBLIC_KEY
+    //               ),
+    //             })
+    //           } else {
+    //             return subscription
+    //           }
+    //         })
+    //     })
+    //     .then((subscription: PushSubscription | null) => {
+    //       if (subscription) {
+    //         const stringifiedSubscription: {
+    //           endpoint: string
+    //           expirationTime: number | null
+    //           keys: {
+    //             p256dh: string
+    //             auth: string
+    //           }
+    //         } = JSON.parse(JSON.stringify(subscription))
+
+    //         createUserDevice({
+    //           webPushEndPoint: stringifiedSubscription.endpoint,
+    //           webPushExpirationTime: stringifiedSubscription.expirationTime,
+    //           webPushKey: stringifiedSubscription.keys.p256dh,
+    //           webPushAuth: stringifiedSubscription.keys.auth,
+    //         })
+    //       }
+    //       console.log('subscription:', subscription)
+    //     })
+    //     .catch((error) => console.error('Service Worker Error', error))
+    // }
   }
 
   return { register }
