01e70ec5d750ee6de2de1638b420e586dcaabdc3
hanzochang <hanzo.chan@gmail.com>
2024-07-16 18:53:00 +0900

    feat push notification

--- hooks/usePushNotificationRegistration.ts
+++ hooks/usePushNotificationRegistration.ts
@@ -9,54 +9,56 @@ export const usePushNotificationRegistration = () => {
     const siwe = await SiweJwtRepository.getSiweJwtFromBrowser()
     if (!siwe) return
 
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log(
-            'Service Worker registered with scope:',
-            registration.scope
-          )
+    console.log('check sw')
+    console.log(navigator.serviceWorker)
+    const swRegistration = await navigator.serviceWorker.register('/sw.js')
 
-          return registration.pushManager
-            .getSubscription()
-            .then((subscription) => {
-              if (subscription === null) {
-                // サブスクリプションを作成
-                // repositoryに移動
-                return registration.pushManager.subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: urlBase64ToUint8Array(
-                    applicationProperties.VAPID_PUBLIC_KEY
-                  ),
-                })
-              } else {
-                return subscription
-              }
-            })
-        })
-        .then((subscription: PushSubscription | null) => {
-          if (subscription) {
-            const stringifiedSubscription: {
-              endpoint: string
-              expirationTime: number | null
-              keys: {
-                p256dh: string
-                auth: string
-              }
-            } = JSON.parse(JSON.stringify(subscription))
+    // test
+    let pushManager = swRegistration.pushManager
+    console.log('pushManager', pushManager)
+    let permissionState = await pushManager.permissionState({
+      userVisibleOnly: true,
+    })
+    console.log('permissionState', permissionState)
+    // test
 
-            createUserDevice({
-              webPushEndPoint: stringifiedSubscription.endpoint,
-              webPushExpirationTime: stringifiedSubscription.expirationTime,
-              webPushKey: stringifiedSubscription.keys.p256dh,
-              webPushAuth: stringifiedSubscription.keys.auth,
-            })
-          }
-          console.log('subscription:', subscription)
-        })
-        .catch((error) => console.error('Service Worker Error', error))
+    console.log('check pushManagerSubscription')
+    let pushManagerSubscription =
+      await swRegistration.pushManager.getSubscription()
+
+    if (pushManagerSubscription) {
+      console.log('pms exists--', pushManagerSubscription)
+    } else {
+      console.log('pms not exists--', pushManagerSubscription)
+      pushManagerSubscription = await swRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          applicationProperties.VAPID_PUBLIC_KEY
+        ),
+      })
+
+      console.log('pms created--', pushManagerSubscription)
     }
+
+    console.log('save subscription')
+    if (pushManagerSubscription) {
+      const stringifiedSubscription: {
+        endpoint: string
+        expirationTime: number | null
+        keys: {
+          p256dh: string
+          auth: string
+        }
+      } = JSON.parse(JSON.stringify(pushManagerSubscription))
+
+      createUserDevice({
+        webPushEndPoint: stringifiedSubscription.endpoint,
+        webPushExpirationTime: stringifiedSubscription.expirationTime,
+        webPushKey: stringifiedSubscription.keys.p256dh,
+        webPushAuth: stringifiedSubscription.keys.auth,
+      })
+    }
+    console.log('subscription:', pushManagerSubscription)
   }
 
   return { register }
--- pages/waitings/index.tsx
+++ pages/waitings/index.tsx
@@ -1,14 +1,18 @@
+import { Button } from '@mantine/core'
 import LGuestUserLayout from '../../components/00_layouts/LGuestUserLayout'
 import { SRecommendSignInScreen } from '../../components/04_screens/SRecommendSignInScreen'
 import { SWaitingsScreen } from '../../components/04_screens/SWaitingsScreen'
 import { Seo } from '../../components/99_seo/waitings/Seo'
 import { applicationProperties } from '../../constants/applicationProperties'
 import { waitingsUrl } from '../../helpers/url.helper'
+import { usePushNotificationRegistration } from '../../hooks/usePushNotificationRegistration'
 import { useAuthenticatedStore } from '../../recoil/authenticatedStore/useAuthenticatedStore'
 import { NextPageWithLayout } from '../_app'
 
 const Page: NextPageWithLayout = () => {
   const { authenticated } = useAuthenticatedStore()
+
+  const { register } = usePushNotificationRegistration()
 
   if (authenticated !== 'authenticated') {
     return (
@@ -17,12 +21,26 @@ const Page: NextPageWithLayout = () => {
         <SRecommendSignInScreen
           redirectUrl={`${applicationProperties.HOSTING_URL}${waitingsUrl()}`}
         />
+        <Button
+          onClick={() => {
+            register()
+          }}
+        >
+          Push登録
+        </Button>
       </>
     )
   }
   return (
     <>
       <SWaitingsScreen />
+      <Button
+        onClick={() => {
+          register()
+        }}
+      >
+        Push登録
+      </Button>
     </>
   )
 }
